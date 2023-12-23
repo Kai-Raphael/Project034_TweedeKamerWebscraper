cf43bccb0c64ad420c844503dff0f70625944749
Kai Raphael <kai.raphael97+gitconfigemail@gmail.com>
2023-12-23 23:27:08 +0100

    UPDATE: trying to add the next page linkt

--- src/webscraper.py
+++ src/webscraper.py
@@ -17,11 +17,24 @@ def scrape_moties():
         # Parse the HTML content of the page
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Find all 'a' tags with class 'h-link-inverse'
-        motie_elements = soup.find_all('div', class_='u-mt--large u-break-inside--avoid-at-print m-card m-card--auto-height')
-        print(f"Motie Elements: {motie_elements}")
+        # get all the cards (div elements) from the first page
+        motie_cards = soup.find_all('div', class_='u-mt--large u-break-inside--avoid-at-print m-card m-card--auto-height')
+        next_page_link = soup.find_all('li', class_='m-pager__item m-pager__item--next')
+        
+        print(f"Motie Elements: {motie_cards}")
+        print("next page link: {next_page_link}")
 
-        motie_objects = getMotionList(motie_elements)
+        motie_objects = getMotionList(motie_cards)
 
     else:
         print(f"Failed to retrieve the page. Status Code: {response.status_code}")
+
+
+
+
+#<li class="m-pager__item m-pager__item--next">
+#          <a href="?cfg=tksearch&amp;fld_prl_kamerstuk=Moties&amp;fld_prl_soort=Motie%20%28gewijzigd/nader%29&amp;fld_tk_categorie=Kamerstukken&amp;qry=%2A&amp;srt=date%3Adesc%3Adate&amp;sta=1&amp;page=1" rel="next">
+#            Volgende pagina
+#          </a>
+#        </li>
+
