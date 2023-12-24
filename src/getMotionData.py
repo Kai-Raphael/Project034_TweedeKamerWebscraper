e0fa0bcf34537ada83762226c22d11e7370a576b
Kai Raphael <kai.raphael97+gitconfigemail@gmail.com>
2023-12-24 19:39:00 +0100

    UPDATE: back to Functional programming because I don't know why I should create a classe of a the motion scraper. The hyperlink retriever function now works but the site seems to be partially down. (moties don't load there specific pages)

--- src/getMotionData.py
+++ src/getMotionData.py
@@ -3,47 +3,61 @@ from getMotionList import getMotionList
 from bs4 import BeautifulSoup
 
 # Function to scrape moties from the Tweede Kamer website
-def get_motion_data(base_url):
+def get_motion_data():
+    
+    # set a max for the testing of the model (real pages contains over ~5.0000 results)
+    Max_number_of_motion_pages  = 2
+
+    # URL of the moties home page
+    base_url = "https://www.tweedekamer.nl/kamerstukken/moties"
 
     # Send an HTTP request to the website of the tweede kamer
     response = requests.get(base_url)
 
-    # set a max for the testing of the model (real pages contains over ~5.0000 results)
-    Max_number_of_motion_pages  = 2
-
     # retrieve all page links
     all_page_links = []
 
-    # Loop through the last 
+    target_page = response
+
+    # Test the function for 2 pages
     for i in range(Max_number_of_motion_pages):
 
+        motie_hyperlinks_resultset, next_page_hyperlink = retrieve_hyperlinks_of_moties_page(target_page)
+        target_page = requests.get(next_page_hyperlink)
+        all_page_links.append(motie_hyperlinks_resultset)
+
+    print(f"RETRIEVED PAGE LINKS: \n {all_page_links}")
+       
+def retrieve_hyperlinks_of_moties_page(response):
         # Check if the request was successful (status code 200) (and if so continue)
         if response.status_code == 200:
-            print(f"request successful: {response.status_code}")
+            print(f"\u2705 request successful: {response.status_code}")
 
             # Parse the HTML content of the page
             soup = BeautifulSoup(response.text, 'html.parser')
 
-            # get all the cards (div elements) from the first gitpage
-            motie_cards = soup.find_all('div', class_='u-mt--large u-break-inside--avoid-at-print m-card m-card--auto-height')
-            
+            # URL of the moties home page
+            base_url = "https://www.tweedekamer.nl/kamerstukken/moties"
+
+            motie_hyperlink_count = 0
+            # Find all the hyperlinks of the different moties
+            motie_hyperlinks_resultset = soup.find_all('h4', class_='u-mt--collapse')
+
+            for result in motie_hyperlinks_resultset:
+
+                hyperlink = base_url + result.find('a')['href']
+                print(f" {str(motie_hyperlink_count).zfill(3)} Motie Hyperlink: {hyperlink}" )
+                motie_hyperlink_count += 1
+
             # Retrieve the hyperlink of the next page
-            next_page_link = base_url + soup.find('li', class_='m-pager__item m-pager__item--next').find('a')['href']
+            next_page_hyperlink = base_url + soup.find('li', class_='m-pager__item m-pager__item--next').find('a')['href']
+            print(f"next page link: {next_page_hyperlink} \n")
 
-            #append it to the page_links list
-            all_page_links.append(next_page_link)
+            return motie_hyperlinks_resultset, next_page_hyperlink
 
-            # Also creat an object of the motion itsel (although this might not be necessary)
-            motie_objects = getMotionList(motie_cards)
-
-            # Print the cards and print the last link
-            print(f"Motie Elements: {motie_cards}")
-            print(f"next page link: {next_page_link}")
+            # Also creat an object of the motion itself (although this might not be necessary)
+            #motie_objects = getMotionList(motie_cards)
 
         else:
-            print(f"Failed to retrieve the page. Status Code: {response.status_code}")
-    
-    return all_page_links
-
-for link in all_page_links:
-    print(link)
+            print(f"\u274C Failed to retrieve the page. Status Code: {response.status_code}")
+ 
--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 from getMotionData import get_motion_data
 
-
-# URL of the moties home page
-base_url = "https://www.tweedekamer.nl/kamerstukken/moties"
-
 #going from a machine learning perspective (C.R.I.S.P model)
 def main():
     #Select Data
-    get_motion_data(base_url)
+    get_motion_data()
 
     #Clean data
 
