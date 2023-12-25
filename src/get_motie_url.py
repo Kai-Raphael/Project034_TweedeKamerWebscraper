652d589910885361fe72bac400e5e58a5c1d5736
Kai Raphael <kai.raphael97+gitconfigemail@gmail.com>
2023-12-25 16:22:13 +0100

    UPDATE: combined the get motion data into get_motie_url and created get_motie_content

---
+++ src/get_motie_url.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+base_url = "https://www.tweedekamer.nl"
+moties_page_url = base_url + "/kamerstukken/moties"
+
+def get_motie_urls(number_of_motion_pages):
+    # Define an empty list to hold all the page links
+    all_page_links = []
+
+    # Send an HTTP request to the website of the tweede kamer for the first moties page
+    target_page = requests.get(moties_page_url)
+
+    # Run for the specified amount of pages
+    for _ in range(number_of_motion_pages):
+        # Send an HTTP request to the moties page
+        response = requests.get(target_page.url)
+        
+        if response.status_code == 200:
+            print(f"\u2705 Request successful: {response.status_code}")
+            html_content = response.text
+        else:
+            print(f"\u274C Failed to retrieve the page. Status Code: {response.status_code}")
+            return all_page_links
+
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # Find all the hyperlinks of the different moties
+        motie_hyperlinks_resultset = soup.find_all('h4', class_='u-mt--collapse')
+
+        # Retrieve each motion page link using list comprehension
+        list_of_motie_links = [base_url + result.find('a')['href'] for result in motie_hyperlinks_resultset]
+
+        # Retrieve the hyperlink of the next page
+        next_page_hyperlink = moties_page_url + soup.find('li', class_='m-pager__item m-pager__item--next').find('a')['href']
+
+        # Put all motie-links in a list
+        all_page_links.append(list_of_motie_links)
+
+        # Make the next page hyperlink the following target page
+        target_page = requests.get(next_page_hyperlink)
+
+    return all_page_links
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from getMotionData import get_motion_data
+from get_motie_url import get_motie_urls
 
 # set a max for the testing of the model (real pages contains over ~5.0000 results)
 NUMBER_OF_PAGES_TO_GO_THROUGH = 2
@@ -7,17 +7,22 @@ NUMBER_OF_PAGES_TO_GO_THROUGH = 2
 #going from a machine learning perspective (C.R.I.S.P model)
 def main():
     #Select Data
-    List_of_Lists_of_page_links = get_motion_data(NUMBER_OF_PAGES_TO_GO_THROUGH)
+    List_of_Lists_of_page_links = get_motie_urls(NUMBER_OF_PAGES_TO_GO_THROUGH)
     
     # Use list comprehension to create a link-list 
     links = [link for page_link_list in List_of_Lists_of_page_links for link in page_link_list]
 
+    # FYI: print each link
     for each_link in links:
-        # FYI
-        print(f"\U0001F50D Link: {each_link}")
+        print(f"\U0001F517 Link: {each_link}")
 
     # FYI: print the # of results 
-    print(f"\U0001F517 Found {len(links)} Links on {NUMBER_OF_PAGES_TO_GO_THROUGH} pages: \n")
+    print(f"\U0001F50D Found {len(links)} Links on {NUMBER_OF_PAGES_TO_GO_THROUGH} pages: \n")
+
+    #Now retrieve the motie page details
+
+    #Then retrieve the motie document
+    # https://www.tweedekamer.nl/downloads/document?id=2023D51182
 
     #Clean data
 
