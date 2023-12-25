81b372f0eda4de0ed9eca9b70ee3c7a025d4448b
Kai Raphael <kai.raphael97+gitconfigemail@gmail.com>
2023-12-25 15:45:46 +0100

    UPDATE: retrieving of links works

--- src/getMotionData.py
+++ src/getMotionData.py
@@ -1,63 +1,81 @@
 import requests
-from getMotionList import getMotionList
 from bs4 import BeautifulSoup
 
-# Function to scrape moties from the Tweede Kamer website
-def get_motion_data():
-    
-    # set a max for the testing of the model (real pages contains over ~5.0000 results)
-    Max_number_of_motion_pages  = 2
+base_url = "https://www.tweedekamer.nl/kamerstukken/moties"
 
+# send a request to a target url
+def send_request(target_url):
+    
+    # Store the response in a variable
+    response = requests.get(target_url)
+
+    # If that response is good (200) return the text
+    if response.status_code == 200:
+        print(f"\u2705 Request successful: {response.status_code}")
+        return response.text, response
+    else:
+        print(f"\u274C Failed to retrieve the page. Status Code: {response.status_code}")
+        return None, response
+
+
+# Function to scrape moties from the Tweede Kamer website
+def get_motion_data(number_of_motion_pages):
+    
     # URL of the moties home page
     base_url = "https://www.tweedekamer.nl/kamerstukken/moties"
 
     # Send an HTTP request to the website of the tweede kamer
-    response = requests.get(base_url)
+    target_page = requests.get(base_url)
 
-    # retrieve all page links
+    # retrieve all page links in a list
     all_page_links = []
 
-    target_page = response
-
     # Test the function for 2 pages
-    for i in range(Max_number_of_motion_pages):
+    for i in range(number_of_motion_pages):
 
+        #retrieve the hyperlinks of the moties and of the next page of the current page
         motie_hyperlinks_resultset, next_page_hyperlink = retrieve_hyperlinks_of_moties_page(target_page)
+
         target_page = requests.get(next_page_hyperlink)
+
         all_page_links.append(motie_hyperlinks_resultset)
-
-    print(f"RETRIEVED PAGE LINKS: \n {all_page_links}")
+    
+    return all_page_links
        
-def retrieve_hyperlinks_of_moties_page(response):
-        # Check if the request was successful (status code 200) (and if so continue)
-        if response.status_code == 200:
-            print(f"\u2705 request successful: {response.status_code}")
+def retrieve_hyperlinks_of_moties_page(target_moties_page):
+    html_content, response = send_request(target_moties_page.url)
 
-            # Parse the HTML content of the page
-            soup = BeautifulSoup(response.text, 'html.parser')
+    print(f"Response: {response.url}")
 
-            # URL of the moties home page
-            base_url = "https://www.tweedekamer.nl/kamerstukken/moties"
+    if html_content:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(html_content, 'html.parser')
 
-            motie_hyperlink_count = 0
-            # Find all the hyperlinks of the different moties
-            motie_hyperlinks_resultset = soup.find_all('h4', class_='u-mt--collapse')
+        # URL of the moties home page
+        base_url = "https://www.tweedekamer.nl"
 
-            for result in motie_hyperlinks_resultset:
+        # Find all the hyperlinks of the different moties
+        motie_hyperlinks_resultset = soup.find_all('h4', class_='u-mt--collapse')
 
-                hyperlink = base_url + result.find('a')['href']
-                print(f" {str(motie_hyperlink_count).zfill(3)} Motie Hyperlink: {hyperlink}" )
-                motie_hyperlink_count += 1
+        list_of_motie_links = []
 
-            # Retrieve the hyperlink of the next page
-            next_page_hyperlink = base_url + soup.find('li', class_='m-pager__item m-pager__item--next').find('a')['href']
-            print(f"next page link: {next_page_hyperlink} \n")
+        # Retrieve each motion page link
+        motie_hyperlink_count = 0
 
-            return motie_hyperlinks_resultset, next_page_hyperlink
+        for result in motie_hyperlinks_resultset:
+            hyperlink = base_url + result.find('a')['href']
+            list_of_motie_links.append(hyperlink)
+            print(f"{str(motie_hyperlink_count).zfill(3)} Motie Hyperlink: {hyperlink}")
+            motie_hyperlink_count += 1
+            
 
-            # Also creat an object of the motion itself (although this might not be necessary)
-            #motie_objects = getMotionList(motie_cards)
 
-        else:
-            print(f"\u274C Failed to retrieve the page. Status Code: {response.status_code}")
- 
+        # Retrieve the hyperlink of the next page
+        next_page_hyperlink = base_url + "/kamerstukken/moties" + soup.find('li', class_='m-pager__item m-pager__item--next').find('a')['href']
+        print(f"next page link: {next_page_hyperlink}\n")
+
+        return list_of_motie_links, next_page_hyperlink
+
+    else:
+        # Handle the case where the request failed
+        return None, None
--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from getMotionData import get_motion_data
 
+# set a max for the testing of the model (real pages contains over ~5.0000 results)
+NUMBER_OF_PAGES_TO_GO_THROUGH = 2
+
+
 #going from a machine learning perspective (C.R.I.S.P model)
 def main():
     #Select Data
-    get_motion_data()
+    List_of_Lists_of_page_links = get_motion_data(NUMBER_OF_PAGES_TO_GO_THROUGH)
+    
+    # Use list comprehension to create a link-list 
+    links = [link for page_link_list in List_of_Lists_of_page_links for link in page_link_list]
+    for each_link in links:
+        print(f"Link: {each_link}")
 
     #Clean data
 
